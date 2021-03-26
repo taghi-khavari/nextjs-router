ef5e709bb89bb453c4d253549861d5f62997e41b
taghi <mtkhavari@gmail.com>
2021-03-26 13:46:36 +0430

    completed the project

---
+++ dummy-data.js
@@ -0,0 +1,60 @@
+const DUMMY_EVENTS = [
+  {
+    id: "e1",
+    title: "Programming for everyone",
+    description:
+      "Everyone can learn to code! Yes, everyone! In this live event, we are going to go through all the key basics and get you started with programming as well.",
+    location: "Somestreet 25, 12345 San Somewhereo",
+    date: "2021-05-12",
+    image:
+      "https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=600&q=80",
+    isFeatured: false,
+  },
+  {
+    id: "e2",
+    title: "Networking for introverts",
+    description:
+      "We know: Networking is no fun if you are an introvert person. That's why we came up with this event - it'll be so much easier. Promised!",
+    location: "New Wall Street 5, 98765 New Work",
+    date: "2021-05-30",
+    image:
+      "https://images.unsplash.com/photo-1565802525957-d818c100d6e8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=80",
+    isFeatured: true,
+  },
+  {
+    id: "e3",
+    title: "Networking for extroverts",
+    description:
+      "You probably need no help with networking in general. But focusing your energy correctly - that is something where most people can improve.",
+    location: "My Street 12, 10115 Broke City",
+    date: "2022-04-10",
+    image:
+      "https://images.unsplash.com/photo-1515169067868-5387ec356754?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8bmV0d29ya2luZ3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
+    isFeatured: true,
+  },
+];
+
+export function getFeaturedEvents() {
+  return DUMMY_EVENTS.filter((event) => event.isFeatured);
+}
+
+export function getAllEvents() {
+  return DUMMY_EVENTS;
+}
+
+export function getFilteredEvents(dateFilter) {
+  const { year, month } = dateFilter;
+
+  let filteredEvents = DUMMY_EVENTS.filter((event) => {
+    const eventDate = new Date(event.date);
+    return (
+      eventDate.getFullYear() === year && eventDate.getMonth() === month - 1
+    );
+  });
+
+  return filteredEvents;
+}
+
+export function getEventById(id) {
+  return DUMMY_EVENTS.find((event) => event.id === id);
+}
--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,8 +1,40 @@
+import { Fragment } from "react";
 import { useRouter } from "next/router";
 
-export default function SingleEvent(props) {
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+import ErrorAlert from "../../components/ui/error-alert";
+
+function EventDetailPage() {
   const router = useRouter();
-  console.log("router in single event is: ", router);
-  const { eventId } = router.query;
-  return <div>I'm the single event with id: {eventId}</div>;
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return (
+      <ErrorAlert>
+        <p>No event found!</p>
+      </ErrorAlert>
+    );
+  }
+
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
 }
+
+export default EventDetailPage;
